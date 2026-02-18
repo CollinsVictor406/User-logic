4272c9cb752af8edabd7c683d2dfefaef9036380
collins <collinsv406@gmail.com>
2026-02-18 12:31:46 +0100

    version 2

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.use('/',(req,res)=>{
-    res.send("Welcome Home Collins Victor Obehioye")
-})
+app.use('/')
+
 app.listen(5000,()=>{
     console.log("App is running on port 5000");
     
